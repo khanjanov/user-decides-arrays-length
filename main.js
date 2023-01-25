2c905bb16f7048afd3a9284a9d92621d506802b2
khanjanov <you@example.com>
2023-01-26 03:22:25 +0400

    eventlistener added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let result = document.querySelector(".container__result");
+let container = document.querySelector(".container");
 let a = prompt("enter array's length");
 let arr = [];
 let i = 0;
@@ -6,3 +7,6 @@ for (i; i < a; i++) {
   arr[i] = +prompt(` enter array's number ${i + 1} `);
 }
 result.innerHTML = `${arr}`;
+container.addEventListener("click", () => {
+  result.innerHTML = "I knew you that would click 😃";
+});
